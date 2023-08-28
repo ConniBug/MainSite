79301dcc6c14e5f5e4ebb591b1138cca55eff461
Conni Bilham <me@conni.lgbt>
2023-08-28 16:08:49 +0100

    Change launch options

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,16 +7,11 @@ const miniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin')
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: './src/js/main.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    },
-    performance: {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000
     },
     plugins: [
         new HtmlWebpackPlugin({ template: './src/index.html' }),
